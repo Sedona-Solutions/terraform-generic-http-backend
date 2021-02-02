2e1725f835de2d29881621cad596a9f5926c8871
Mathieu Broutin <mbroutin@sedona.fr>
2021-02-02 10:56:19 +0100

    feat(model): use correct case for attributes

--- src/main/kotlin/fr/sedona/terraform/http/model/TfLockInfo.kt
+++ src/main/kotlin/fr/sedona/terraform/http/model/TfLockInfo.kt
@@ -1,5 +1,6 @@
 package fr.sedona.terraform.http.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
 /**
@@ -11,36 +12,43 @@ data class TfLockInfo(
      * be overridden by the lock implementation. The final value if ID will be
      * returned by the call to Lock.
      */
+    @JsonProperty("ID")
     val id: String?,
 
     /**
      * Terraform operation, provided by the caller.
      */
+    @JsonProperty("Operation")
     val operation: String?,
 
     /**
      * Extra information to store with the lock, provided by the caller.
      */
+    @JsonProperty("Info")
     val info: String?,
 
     /**
      * user@hostname when available
      */
+    @JsonProperty("Who")
     val who: String?,
 
     /**
      * Terraform version
      */
+    @JsonProperty("Version")
     val version: String?,
 
     /**
      * Time that the lock was taken.
      */
+    @JsonProperty("Created")
     // FIXME Created *time.Time `json:"created,omitempty"`
     val created: Date?,
 
     /**
      * Path to the state file when applicable. Set by the Lock implementation.
      */
+    @JsonProperty("Path")
     val path: String?
 )
